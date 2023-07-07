3b3cf53efb2df6b8a782cbf33f29e8253ec2ce04
Ant Stanley <138448663+ant-akeero@users.noreply.github.com>
2023-07-07 07:57:56 +0200

    feat: remove gfc dependency

--- test/test.js
+++ test/test.js
@@ -1,27 +1,34 @@
 'use strict';
 
-const gfc = require('gfc');
+const { spawnSync } = require('node:child_process');
 const path = require('path');
-const util = require('util');
 const assert = require('assert');
-const rimraf = util.promisify(require('rimraf'));
+const fs = require('fs')
 const branch = require('..');
-const fixtures = path.join(__dirname, 'fixtures');
+const fixtures = path.join(process.cwd(), 'fixtures');
 
-const create = async() => await rimraf(fixtures).then(() => gfc(fixtures));
+function create(folderName) {
+  if (fs.existsSync(folderName)) fs.rmSync(folderName, { recursive: true })
+  fs.mkdirSync(folderName)
+  spawnSync('git', ['init', '-q', '--initial-branch=main'], { cwd: folderName })
+}
+
+function cleanUp(folderName) {
+  if (fs.existsSync(folderName)) fs.rmSync(folderName, { recursive: true })
+}
+
+before(() => create(fixtures));
+after(() => cleanUp(fixtures));
 
 describe('git-branch', function() {
-  beforeEach(() => create());
-  afterEach(() => rimraf(fixtures));
-
-  it('should get branch (sync)', () => assert.equal(branch.sync(fixtures), 'master'));
-  it('should get branch (promise)', function() {
-    return branch(fixtures).then(res => assert.equal(res, 'master'));
+  it('should get branch (sync)', () => assert.equal(branch.sync(fixtures), 'main'));
+  it('should get branch (promise)', async () => {
+    return branch(fixtures).then(res => assert.equal(res, 'main'));
   });
   it('should get branch (async)', function(cb) {
     branch(fixtures, function(err, res) {
       if (err) return cb(err);
-      assert.equal(res, 'master');
+      assert.equal(res, 'main');
       cb();
     });
   });
